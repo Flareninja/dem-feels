9707c33873fbb4696ea4cf57174aae0156a2f8c2
Phil <birkal@outlook.com>
2016-02-06 22:58:38 -0800

    Add tests

---
+++ test/index.js
@@ -0,0 +1,69 @@
+const test = require('tape');
+const demFeels = require('../src');
+
+// ei shorthand for emoteImage
+function ei(name) {
+  return '<img src="https://cdn.rawgit.com/CreaturePhil/dem-feels/master/' + name + '.png" title="' + name + '" width="50" height="50" />'
+}
+
+test('parse a single emote', function(t) {
+  t.plan(2);
+
+  t.equal(demFeels('feelsgd'), ei('feelsgd'));
+
+  t.equal(demFeels('feelsbd'), ei('feelsbd'));
+});
+
+test('parse multiple emotes', function(t) {
+  t.plan(2);
+
+  t.equal(demFeels('feelsgd feelsgd'), ei('feelsgd') + ' ' + ei('feelsgd'));
+
+  t.equal(demFeels('feelsgd feelsbd'), ei('feelsgd') + ' ' + ei('feelsbd'));
+});
+
+test('dont parse emote if it is not seperated by a spaces', function(t) {
+  t.plan(2);
+
+  t.equal(demFeels('feelsgdfeelsgd'), 'feelsgdfeelsgd');
+
+  t.equal(demFeels(' feelsgdfeelsgd '), 'feelsgdfeelsgd');
+});
+
+test('parse emote with other text', function(t) {
+  t.plan(3);
+
+  t.equal(demFeels('hi feelsgd'), 'hi ' + ei('feelsgd'));
+
+  t.equal(demFeels('feelsgd bye'), ei('feelsgd') + ' bye');
+
+  t.equal(demFeels('hi feelsgd bye'), 'hi '+ ei('feelsgd') + ' bye');
+});
+
+test('parse maximum emotes', function(t) {
+  t.plan(2);
+
+  const fg = ' ' + ei('feelsgd');
+  const fb = ' ' + ei('feelsbd');
+
+  t.equal(demFeels('hello feelsgd feelsgd feelsgd feelsgd feelsgd'), 'hello' + fg + fg + fg + fg + fg);
+
+  t.equal(demFeels('sup feelsgd feelsgd feelsbd feelsgd feelsgd yo feelsgd feelsbd see ya'), 'sup' + fg + fg + fb + fg + fg + ' yo feelsgd feelsbd see ya');
+});
+
+test('increasing maxCount limit', function(t) {
+  t.plan(1);
+
+  demFeels.setMaxCount(10);
+
+  const fg = ' ' + ei('feelsgd');
+  const fb = ' ' + ei('feelsbd');
+
+  t.equal(demFeels('sup feelsgd feelsgd feelsbd feelsgd feelsgd yo feelsgd feelsbd see ya'), 'sup' + fg + fg + fb + fg + fg + ' yo' + fg + fb + ' see ya');
+});
+
+test('getting emotes', function(t) {
+  t.plan(1);
+
+  t.equal(typeof demFeels.getEmotes(), 'object');
+});

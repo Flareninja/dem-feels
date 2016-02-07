32624a7df26cc604bf59e57d79b3a1cbdf6e6496
Phil <birkal@outlook.com>
2016-02-06 22:26:36 -0800

    Add completed api

--- src/index.js
+++ src/index.js
@@ -1,66 +1,22 @@
 'use strict';
 
-const R = require('ramda');
-
-const compose = R.compose;
-const curry = R.curry;
-const head = R.head;
-const last = R.last;
-const indexOf = R.indexOf;
-const join = R.join;
-const prop = R.prop;
-const replace = R.replace;
-const slice = R.slice;
-const split = R.split;
-const tail = R.tail;
-const toLower = R.toLower;
-const trim = R.trim;
-
 const makeEmoteMap = require('./makeEmoteMap');
+const makeParseMessage = require('./makeParseMessage');
 
-// imgTag :: String
-const imgTag = '<img src="{url}" title="{name}" width="50" height="50" />';
+const emotes = makeEmoteMap.unsafePerformIO();
 
-// parseEmote :: String -> String -> String
-const parseEmote = function(url, name) {
-  const template = compose(replace('{name}', name), replace('{url}', url));
-  return template(imgTag);
+let maxCount = 5;
+
+function demFeels(msg) {
+  return makeParseMessage(emotes, maxCount, msg);
+}
+
+demFeels.getEmotes = function() {
+  return emotes;
 };
 
-console.log(parseEmote('lol.png', 'phil'))
+demFeels.setMaxCount = function(count) {
+  maxCount = count;
+};
 
-// msgToList :: String -> String
-const msgToList = compose(split(' '), trim);
-
-
-// makeParseMessage :: String -> String
-const makeParseMessage = curry(function(msg, defaultMaxCount, emotes) {
-  const list = msgToList(msg);
-
-  const parsed = list.reduce(function(acc, cur) {
-    const token = toLower(cur);
-    const isEmote = prop(token, emotes);
-    console.log(isEmote, acc.count, token)
-    if (isEmote && acc.count < defaultMaxCount) {
-      const p = slice(0, indexOf(token, acc.list) + 1, acc.list);
-      const o = replace(token, parseEmote(isEmote, token), join(' ', p))
-      //console.log(p, o);
-      return {
-        list: tail(acc.list),
-        count: acc.count + 1,
-        output: acc.output + ' ' + o
-      };
-    } else {
-      return {
-        list: tail(acc.list),
-        count: acc.count,
-        output: acc.output + ' ' + cur
-      };
-    }
-  }, {list: list, count: 0, output: ''});
-console.log('\n\n')
-  return parsed.output
-});
-const emotes = makeEmoteMap.unsafePerformIO();
-console.log(emotes);
-console.log(makeParseMessage('hello feelsgd feelsgd yo feelsgd feelsgd feelsgd hmm feelsgd', 5, emotes));
+module.exports = demFeels;
